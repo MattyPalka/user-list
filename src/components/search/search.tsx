0522870cf44c8e08255abe82afc358dc13f585f2
Matt <mateuszpalka90@gmail.com>
2021-06-11 16:56:33 +0200

    input styling

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -6,15 +6,35 @@ import style from "./style.module.scss";
 const Search = () => {
   const dispatch = useDispatch();
   const [value, setValue] = useState("");
+  const [shrinkLabel, setShrinkLabel] = useState(false);
   useEffect(() => {
     dispatch(filterUsers(value));
   }, [value]);
+
+  /**
+   * Set input label to either be in input or above it (when has value or is in focus) for better UX
+   * @param event type of action event
+   */
+  const toggleLabel = (event: { type: string }) => {
+    if (event.type === "focus") {
+      setShrinkLabel(true);
+    } else {
+      if (!value) {
+        setShrinkLabel(false);
+      }
+    }
+  };
+
   return (
     <div className={style.container}>
-      <label>Search by user name...</label>
+      <label className={shrinkLabel ? style.withInput : ""}>
+        Search by user name...
+      </label>
       <input
         className={style.searchInput}
         value={value}
+        onFocus={toggleLabel}
+        onBlur={toggleLabel}
         onChange={(e) => {
           setValue(e.target.value);
         }}

81ddb9d25d12c93a2b354536b36a18c9286da5d3
Matt <mateuszpalka90@gmail.com>
2021-06-11 17:46:31 +0200

    fixes to search styling

--- src/components/users/single-user/single-user.tsx
+++ src/components/users/single-user/single-user.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { user } from "../../../types/user";
 import style from "./style.module.scss";
 
 const SingleUser = ({ user }: { user: user }) => {
+  const [showDetail, setShowDetail] = useState(false);
+
   return (
     <li className={style.userListItem}>
-      {user.name} <span className={style.username}>@{user.username}</span>
+      <div>
+        {user.name} <span className={style.username}>@{user.username}</span>
+      </div>
     </li>
   );
 };
